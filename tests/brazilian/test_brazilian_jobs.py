7f7784022257541beda0a906fdf3892481076eed
gabsgregorio <gabsufrrj@gmail.com>
2022-10-25 11:22:58 -0300

    feat: implementa o test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for entry in jobs:
+        assert ("title" in entry) is True
+        assert ("salary" in entry) is True
+        assert ("type" in entry) is True
